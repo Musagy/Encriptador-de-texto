ffcb4fd90638ed44dcce5c580d778ce50d3c465c
Musagy <diegomusagy@gmail.com>
2023-04-12 11:34:24 -0500

    Add commentaries in script 🐳

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { encrypt, decrypt } from "./utils/encrypt.js"
 
-// Localizo elementos principales
+// LOCALIZO ELEMENTOS PRINCIPALES
 const input = document.getElementById("inputMain")
 const result = document.getElementById("result")
 
@@ -9,7 +9,7 @@ const decryptBtn = document.getElementById("decryptBtn")
 
 const copyBtn = document.getElementById("copyBtn")
 
-// Configuro variables y estados
+// CONFIGURO VARIABLES Y ESTADOS
 const inputSaved = localStorage.getItem("inputSaved")
 const resultSaved = localStorage.getItem("resultSaved")
 
@@ -18,44 +18,60 @@ const resultContentDefault = `
 <h2>Ningún mensaje fue encontrado</h2>
 <p>Ingresa el texto que desees encriptar o desencriptar.</p>
 `
-if (inputSaved) input.value = inputSaved
-if (!resultSaved) copyBtn.style.display = "none"
 
-const setResult = text => {
-  result.innerHTML = text
-    ? `<h2 class="resultText">${text}</h2>`
-    : resultContentDefault
-  text
-    ? localStorage.setItem("resultSaved", text)
-    : localStorage.removeItem("resultSaved")
-}
-const updateInput = () => {
-  input.value
-    ? localStorage.setItem("inputSaved", input.value)
-    : localStorage.removeItem("inputSaved")
-}
+// Actualiza la visualización del botón copiar si es que hay resultado
 const updateCopyBtn = () => {
   copyBtn.style.display = input.value ? "block" : "none"
 }
 
-setResult(resultSaved)
+// Función para automatizar el guardado del resultado para la pagina como para el estado
+const setResult = text => {
+  // Inserta el resultado en el contenedor del resultado, o si no pone el contenido por defecto
+  result.innerHTML = text
+    ? `<h2 class="resultText">${text}</h2>`
+    : resultContentDefault
 
-// Creación de funciones de los botones
-const encryptSubmit = () => {
-  setResult(encrypt(input.value))
+  // Guarda el estado o lo elimina de no encontrar nada (para evitar problemas con null)
+  text
+    ? localStorage.setItem("resultSaved", text)
+    : localStorage.removeItem("resultSaved")
+  
+  // Actualiza el botón
   updateCopyBtn()
+}
+
+// Función para automatizar el guardado de lo del input en el estado cada ves que se interactúa con el mismo
+const updateInput = () => {
+  // Guarda o elimina estado si no tiene (para evitar problemas con null otra vez xD)
+  input.value
+    ? localStorage.setItem("inputSaved", input.value)
+    : localStorage.removeItem("inputSaved")
+}
+
+// Configura los estados cuando se inicia la pagina
+if (inputSaved) input.value = inputSaved // Coloca el content del input guardado en el input
+if (!resultSaved) copyBtn.style.display = "none" // Oculta el Botón de guardado, si no hay resultado guardados
+setResult(resultSaved) // Coloca el contenido de resultado guardado en el contenedor si es que tiene
+
+// CREACIÓN DE FUNCIONES DE LOS BOTONES
+const encryptSubmit = () => {
+  // Encripta y el resultado lo pasa al contenedor de resultados 
+  setResult(encrypt(input.value))
 }
 
 const decryptSubmit = () => {
+  // Desencripta y el resultado lo pasa al contenedor de resultados 
   setResult(decrypt(input.value))
-  updateCopyBtn()
 }
 const copyText = () => {
+  // Busca la copia del resultado guardado en el estado
   const result = localStorage.getItem("resultSaved")
+
+  // Lo copia a tu portapapeles
   navigator.clipboard.writeText(result)
 }
 
-// Configurando botones con sus funciones
+// CONFIGURANDO BOTONES CON SUS FUNCIONES
 encryptBtn.onclick = encryptSubmit
 decryptBtn.onclick = decryptSubmit
 copyBtn.onclick = copyText
--- utils/encrypt.js
+++ utils/encrypt.js
@@ -3,23 +3,45 @@ const keys = {
   e: "enter",
   i: "imes",
   o: "ober",
-  u: "ufat"
+  u: "ufat",
 }
 
-export const encrypt = (text) => {
+export const encrypt = text => {
+  // Separa la oración en un array con todos sus caracteres
   const letters = text.split("")
-  const encrypted = letters.map(l => {
-    return keys[l] || l
-  }).join("")
-  return(encrypted)
+
+  // Itera las letras y cada return sera un elemento para un array
+  const encrypted = letters
+    .map(l => {
+      // Esto ve si la letra coincide con algún indice o key del objeto keys
+      // Si coincide con alguno, dará la palabra encriptada.
+      // Si no, solo devolverá la misma letra
+      return keys[l] || l
+    })
+    .join("") // el join une todos los elementos del array a un string
+
+  // retorna la oración encriptada
+  return encrypted
 }
 
-export const decrypt = (text) => {
-  const keysArray = Object.entries(keys);
+export const decrypt = text => {
+  // Hace una copia del objeto keys, pero como un array de array que contiene [Llave, valor]
+  const keysArray = Object.entries(keys)
+
+  // Variable para guardar los resultados de cada búsqueda de palabras clave
   let result = text
-  keysArray.forEach(([key,value])=> {
-    const regex = new RegExp(value, "gi");
+
+  // Itera el array con las [llave, valor]
+  keysArray.forEach(([key, value]) => {
+    // Se crea una expresión regular para buscar la palabra en toda la oración
+    // No importa en ella hay 2 palabras clave, que las va a localizar
+    const regex = new RegExp(value, "gi")
+
+    // Busca las palabras con el regex, y las remplaza por la llave
+    // O sea la letra original
     result = result.replace(regex, key)
   })
-  return(result)
+
+  // Retorna el resultado xD
+  return result
 }
